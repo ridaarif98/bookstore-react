be1557971a779fd13011ad8730894afe107c1063
ridaarif98 <ridaarif16@gmail.com>
2021-09-30 13:29:33 +0200

    Fix linter errors

--- src/redux/api/api.js
+++ src/redux/api/api.js
@@ -4,7 +4,7 @@ import { addBook, removeBook, setBooks } from '../books/books';
 export const loadBooks = async (dispatch) => {
   const booksGet = await axios
     .get(
-      'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/KP5PI7hEkMIt6Rn76UzC/books'
+      'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/KP5PI7hEkMIt6Rn76UzC/books',
     )
     .then((res) => res.data);
   dispatch(setBooks(booksGet));
@@ -14,7 +14,7 @@ export const createBook = (newBook, dispatch) => {
   axios
     .post(
       'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/KP5PI7hEkMIt6Rn76UzC/books',
-      newBook
+      newBook,
     )
     .then((res) => {
       if (res.status === 201) {
@@ -27,7 +27,7 @@ export const createBook = (newBook, dispatch) => {
 export const deleteBook = (btnId, dispatch) => {
   axios
     .delete(
-      `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/KP5PI7hEkMIt6Rn76UzC/books/${btnId}`
+      `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/KP5PI7hEkMIt6Rn76UzC/books/${btnId}`,
     )
     .then((res) => {
       if (res.status === 201) {
